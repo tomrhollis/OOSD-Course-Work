a4734f93e4a670945ec65c5c0a2084f181d9bd9c
tomrhollis <57675191+tomrhollis@users.noreply.github.com>
2020-01-31 15:58:36 -0700

    Notes & Lab 1 (mostly done)

---
+++ ASP.NET/Lab 1 - Temperature Converter/Lab 1 - Temperature Converter/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab_1___Temperature_Converter.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Temperature Converter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/site.css" rel="stylesheet" />
+    <script src="Scripts/jquery-3.4.1.min.js"></script>
+    <script src="Scripts/bootstrap.js"></script>
+</head>
+<body class="container">
+    <header class="jumbotron text-center">
+        <h1>Temperature 
+            <asp:Image ID="Image1" runat="server" Height="146px" ImageUrl="~/Images/Capture.PNG" Width="62px" />
+&nbsp;Conversion</h1>
+    </header>
+    <main>
+        <form id="form1" class="form-horizontal" runat="server">
+            <div class="form-row">
+                <div class="form-group col-sm-6">
+                    <label for="ddlFrom">From:</label>
+                    <asp:DropDownList ID="ddlFrom" class="form-control col-sm-6" runat="server">
+                        <asp:ListItem Value="C">Celsius</asp:ListItem>
+                        <asp:ListItem Value="F">Fahrenheit</asp:ListItem>
+                        <asp:ListItem Value="K">Kelvin</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                <div class="form-group col-sm-6">
+                    <label for="ddlTo">To:</label>
+                    <asp:DropDownList ID="ddlTo" class="form-control col-sm-6" runat="server">
+                        <asp:ListItem Value="F">Fahrenheit</asp:ListItem>
+                        <asp:ListItem Value="C">Celsius</asp:ListItem>
+                        <asp:ListItem Value="K">Kelvin</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-sm-6">
+                    <label for="txtInput">Input:</label>
+                    <asp:TextBox ID="txtInput" class="form-control col-sm-6" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" class="validator" runat="server" ErrorMessage="RequiredFieldValidator" Display="Dynamic" ControlToValidate="txtInput" Text="Input is a required field"></asp:RequiredFieldValidator>
+                    <asp:RangeValidator ID="RangeValidator1" runat="server" class="validator" ErrorMessage="Input must be between -1000 and 1000" Display="Dynamic" ControlToValidate="txtInput" MinimumValue="-1000" MaximumValue="1000" Type="Double"></asp:RangeValidator>
+                </div>
+                <div class="form-group col-sm-6">
+                    <label>Answer:</label>
+                    <asp:Label ID="lblAnswer" class="form-control col-sm-6" runat="server" Text=""></asp:Label>
+                </div>
+            </div>
+            <div class="form-row">
+                <asp:Button ID="btnCalc" class="col-sm-3 btn" runat="server" Text="Calculate" OnClick="btnCalc_Click" />
+                <div class="col-sm-3"></div>
+                <asp:Button ID="btnClear" class="col-sm-3 btn" runat="server" Text="Clear" OnClick="btnClear_Click" />
+            </div>
+        </form>
+    </main>
+</body>
+</html>
---
+++ ASP.NET/Lab 1 - Temperature Converter/Lab 1 - Temperature Converter/Default.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab_1___Temperature_Converter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Clear the fields and reset drop downs to default
+        /// </summary>
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            lblAnswer.Text = "";
+            txtInput.Text = "";
+            ddlFrom.SelectedValue = "C";
+            ddlTo.SelectedValue = "F";
+        }
+
+        /// <summary>
+        /// Decide what calculation we need to use, and display the result
+        /// </summary>
+        protected void btnCalc_Click(object sender, EventArgs e)
+        {
+            double input = Convert.ToDouble(txtInput.Text);   // the input temperature (relying on ASP.NET validation tags)
+            double output = 0;                    // the result of the calculation
+            string from = ddlFrom.SelectedValue;  // the character value of the first dropdown selection
+            string to = ddlTo.SelectedValue;      // the character value of the second dropdown selection
+
+
+            switch (from)
+            {
+                case "C":
+                    switch (to)
+                    {
+                        case "F":
+                            output = CtoF(input);
+                            break;
+                        case "K":
+                            output = CtoK(input);
+                            break;
+                        case "C":   
+                            output = input;
+                            break;
+                    }
+                    break;
+                case "F":
+                    switch (to)
+                    {
+                        case "C":
+                            output = FtoC(input);
+                            break;
+                        case "K":
+                            output = FtoK(input);
+                            break;
+                        case "F":    
+                            output = input;
+                            break;
+                    }
+                    break;
+                case "K":
+                    switch (to)
+                    {
+                        case "F":
+                            output = KtoF(input);
+                            break;
+                        case "C":
+                            output = KtoC(input);
+                            break;
+                        case "K":  
+                            output = input;
+                            break;
+                    }
+                    break;
+
+            }
+
+            lblAnswer.Text = output.ToString("F");
+            
+        }
+
+
+
+        /// <summary>
+        /// convert celsius to kelvin
+        /// </summary>
+        /// <param name="temp">The temperature to convert, in Celsius</param>
+        /// <returns>the temperature in kelvin</returns>
+        private double CtoK(double temp)
+        {
+            return (temp + 273.15);
+        }
+
+        /// <summary>
+        /// convert fahrenheit to kelvin
+        /// </summary>
+        /// <param name="temp">The temperature to convert, in fahrenheit</param>
+        /// <returns>the temperature in kelvin</returns>
+        private double FtoK(double temp)
+        {
+            //just use celsius as a middleman
+            return CtoK(FtoC(temp));
+        }
+
+        /// <summary>
+        /// convert kelvin to celsius
+        /// </summary>
+        /// <param name="temp">The temperature to convert, in kelvin</param>
+        /// <returns>the temperature in celsius</returns>
+        private double KtoC(double temp)
+        {
+            return (temp - 273.15);
+        }
+
+        /// <summary>
+        /// convert kelvin to fahrenheit
+        /// </summary>
+        /// <param name="temp">The temperature to convert, in kelvin</param>
+        /// <returns>the temperature in Fahrenheit</returns>
+        private double KtoF(double temp)
+        {
+            //just use celsius as a middleman
+            return CtoF(KtoC(temp));
+        }
+
+        /// <summary>
+        /// convert celsius to fahrenheit
+        /// </summary>
+        /// <param name="temp">The temperature to convert, in Celsius</param>
+        /// <returns>the temperature in Fahrenheit</returns>
+        private double CtoF(double temp)
+        {
+            return ((temp * 9 / 5) + 32);
+        }
+
+        /// <summary>
+        /// convert fahrenheit to celsius
+        /// </summary>
+        /// <param name="temp">The temperature to convert, in Fahrenheit</param>
+        /// <returns>the temperature in Celsius</returns>
+        private double FtoC(double temp)
+        {
+            return ((temp - 32) * 5 / 9);
+        }
+    }
+}
